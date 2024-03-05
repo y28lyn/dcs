ee91b169b8a4b2cf90f7190ee0fd136c73df006b
y28lyn <i.beirade@gmail.com>
2024-03-05 13:44:05 +0100

    feat: :sparkles: Initializing project

---
+++ includes/connexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'association';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+?>
